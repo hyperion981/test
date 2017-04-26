4911069fca2b5a798cc174e09c940c2af77f98ba
unknown <>
2017-04-26 19:29:31 +0800

    Initial commit

---
+++ baidu_search.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+
+from selenium import webdriver
+
+import time
+
+driver = webdriver.Chrome(executable_path=r"D:\chromedriver.exe")
+driver.get("http://www.baidu.com")
+
+search_box = driver.find_element_by_id("kw")
+search_box.send_keys("s")
+
+search_click = driver.find_element_by_id("su")
+search_click.click()
+#driver.qiut()
---
+++ clean_p_test.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+
+import os
+import time
+
+count_time=0
+
+for num_time in range(100):
+    os.system('adb shell input keyevent 223')
+    time.sleep(2)
+    os.system('adb shell input keyevent 224')
+    time.sleep(1)
+    os.system('adb shell input swipe 300 1000 300 500')
+    time.sleep(3)
+    count_time += 1
